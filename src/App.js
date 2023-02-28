8559f21a3bfbadc0f7a34be0edeeaca5145c38e7
Steph <steph@oceans404.com>
2023-02-28 11:35:02 -0700

    Starter repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Container from "@mui/material/Container";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const incrementCounter = () => {
+    // we should read currentCount from the blockchain
+    const currentCount = count;
+    setCount(currentCount + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <Card sx={{ minWidth: 275, marginTop: 20 }}>
+        <CardContent>
+          <p>Count: {count}</p>
+          <Button onClick={incrementCounter} variant="outlined">
+            +1
+          </Button>
+        </CardContent>
+      </Card>
+    </Container>
   );
 }
 
